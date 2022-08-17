626b4dff291714f8c50ec0a2d17c83ba54f543ac
hairymax <aleshin.m.s@gmail.com>
2022-08-17 17:45:55 +1000

    data_preparation script

--- data_preparation.py
+++ data_preparation.py
@@ -1,21 +1,44 @@
 import os
 from tqdm import tqdm as tqdm
 import pandas as pd
+import argparse
 
 from src.image_io import read_image, save_image
 
-ORG_DIR = 'CelebA_Spoof/'
-CROP_DIR = 'CelebA_Spoof_crop/' 
+#ORIG_DIR = 'CelebA_Spoof/'
+#CROP_DIR = 'CelebA_Spoof_crop/' 
+#spoof_types = [0, 1, 2, 3, 7, 8, 9] # Spoof атаки, которые оставляем
 
-# Spoof атаки, которые оставляем
-spoof_filter = [0, 1, 2, 3, 7, 8, 9]
-size = 120
+def parse_args():
+    """parsing arguments"""
+    
+    def check_zero_to_one(value):
+        fvalue = float(value)
+        if fvalue < 0 or fvalue > 1:
+            raise argparse.ArgumentTypeError("%s is an invalid value" % value)
+        return fvalue
+    
+    p = argparse.ArgumentParser(description="Cropping images by bbox")
+    p.add_argument("--orig_dir", type=str, default="CelebA_Spoof/", 
+                   help="Directory with original Celeba_Spoof dataset")
+    p.add_argument("--crop_dir", type=str, default="CelebA_Spoof_crop/",
+                   help="Directory to save cropped dataset")
+    p.add_argument("--size", type=int, default=256,
+                   help="Size of the largest side of the image, px")
+    p.add_argument("--bbox_inc", type=check_zero_to_one, default=0.3,
+                   help="Image bbox increasing")
+    p.add_argument("--spoof_types", type=int, nargs="+", default=list(range(10)),
+                   help="Spoof types to keep")
+    args = p.parse_args()
 
-def process_images(labels, size, scaleup=False):
+    return args
+
+
+def process_images(orig_dir, crop_dir, labels, size, scaleup=False, bbox_inc = 0.3):
     for img_path in tqdm(labels):
-        img = read_image(ORG_DIR+img_path, bbox_inc = 0.4)
+        img = read_image(orig_dir+img_path, bbox_inc=bbox_inc)
 
-        new_img_path = img_path.replace('Data', CROP_DIR+'data'+str(size))
+        new_img_path = img_path.replace('Data', crop_dir+'data'+str(size))
         new_img_dir = os.path.dirname(new_img_path)
 
         if not os.path.exists(new_img_dir):
@@ -23,33 +46,47 @@ def process_images(labels, size, scaleup=False):
         save_image(new_img_path, img, size, scaleup=scaleup)
         
 
-org_lbl_dir = ORG_DIR + 'metas/intra_test/'
+if __name__ == "__main__":
+    args = parse_args()
+    print('Check arguments:')
+    print('    Original dataset directory       :', args.orig_dir)
+    print('    Directory to save cropped images :', args.crop_dir+'data'+str(args.size))
+    print('    Spoof types to keep in dataset   :', args.spoof_types)
+    print('    Crop size, bbox increasing       :', (args.size, args.bbox_inc))
+    
+    proceed = input('\nProceed? [y/n] : ').lower()[:1] == 'y'
+    if proceed:
+        org_lbl_dir = args.orig_dir + 'metas/intra_test/'
 
-# read labels
-print('\nReading labels...')
-train_label = pd.read_json(org_lbl_dir+'train_label.json', orient='index'
-                ).apply(pd.to_numeric, downcast='integer')[:1000]
-test_label = pd.read_json(org_lbl_dir+'test_label.json', orient='index'
-                ).apply(pd.to_numeric, downcast='integer')[:100]
-print('Train / Test shape {} / {}:'.format(train_label.shape, test_label.shape))
+        # read labels
+        print('\nReading labels...')
+        train_label = pd.read_json(org_lbl_dir+'train_label.json', orient='index'
+                       ).apply(pd.to_numeric, downcast='integer')#[:10]
+        test_label  = pd.read_json(org_lbl_dir+'test_label.json', orient='index'
+                       ).apply(pd.to_numeric, downcast='integer')#[:10]
+        print('Train / Test shape : {} / {}:'.format(train_label.shape, test_label.shape))
 
-# filter dataset with specified spoof types
-print('\nFiltering labels...')
-train_label = train_label[train_label[40].isin(spoof_filter)]
-test_label  = test_label[test_label[40].isin(spoof_filter)]
-print('Train / Test shape {} / {}:'.format(
-    train_label.shape, test_label.shape))
+        # filter dataset with specified spoof types
+        print('\nFiltering labels...')
+        train_label = train_label[train_label[40].isin(args.spoof_types)]
+        test_label  = test_label[test_label[40].isin(args.spoof_types)]
+        print('Train / Test shape : {} / {}:'.format(train_label.shape, test_label.shape))
 
-# Read, Crop, Save images
-print('\nProcessing train data...') 
-process_images(train_label.index, size, scaleup=False)
-print('\nProcessing test data...')
-process_images(test_label.index, size, scaleup=False)
+        # Read, Crop, Save images
+        print('\nProcessing train data...') 
+        process_images(args.orig_dir, args.crop_dir, 
+                       train_label.index, args.size, scaleup=False)
+        print('\nProcessing test data...')
+        process_images(args.orig_dir, args.crop_dir, 
+                       test_label.index, args.size, scaleup=False)
 
-# write labels
-print('\nWriting labels')
-train_label.index = train_label.index.str.replace('Data/', '')
-test_label.index = test_label.index.str.replace('Data/', '')
-pd.concat([train_label, test_label]).to_csv(CROP_DIR+'data'+str(size)+'/label.csv')
+        # write labels
+        print('\nWriting labels...')
+        train_label.index = train_label.index.str.replace('Data/', '')
+        test_label.index  = test_label.index.str.replace('Data/', '')
+        pd.concat([train_label, test_label]).to_csv(args.crop_dir+'data'+str(args.size)+'/label.csv')
 
-print('Finished\n')
+        print('\nFinished\n')
+    
+    else:
+        print('\nCancelled\n')
