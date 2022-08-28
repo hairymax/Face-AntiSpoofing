0566a4c6419076a5cb2e0e0967377a5a793a809a
hairymax <aleshin.m.s@gmail.com>
2022-08-29 01:00:38 +1000

    downsampling class balancing

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
 from src import config as cfg
 from src.train_main import TrainMain
 
-cnf = cfg.get_train_config()
-cnf = cfg.set_train_job(cnf, 'AntiSpoofing_bin')
+cnf = cfg.get_train_config(spoof_categories=[[0],[1,2,3],[7,8,9]],
+                           class_balancing='down')
+print(cnf.num_classes)
+cnf = cfg.set_train_job(cnf, 'AntiSpoofing_print_replay_cb-down')
 
-trainer = TrainMain(cnf)
-trainer.train_model()
+# trainer = TrainMain(cnf)
+# trainer.train_model()
